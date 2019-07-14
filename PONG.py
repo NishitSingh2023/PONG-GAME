4128579a2cf35559ae90559eaa9b0a5df27aca90
Nishit Singh <ns2023@gmail.com>
2019-07-14 14:49:10 +0530

    Completing The Screen

--- PONG.py
+++ PONG.py
@@ -23,4 +23,50 @@ area.goto(play_right, play_bottom)
 area.goto(play_left, play_bottom)
 area.goto(play_left, play_top)
 
+# BALL
+ball = Turtle()
 
+ball.penup()
+ball.shape("circle")
+ball.shapesize(0.5, 0.5)
+
+ball_radius = 10 * 0.5
+
+# Paddles
+L = Turtle()
+R = Turtle()
+L.penup()
+R.penup()
+
+# Paddle shape
+paddle_w_half = 10 / 2      # 10 units wide
+paddle_h_half = 40 / 2      # 40 units high
+paddle_shape = Shape("compound")
+paddle_points = ((-paddle_h_half, -paddle_w_half),
+                 (-paddle_h_half, paddle_w_half),
+                 (paddle_h_half, paddle_w_half),
+                 (paddle_h_half, -paddle_w_half))
+paddle_shape.addcomponent(paddle_points, "black")
+screen.register_shape("paddle", paddle_shape)
+L.shape("paddle")
+R.shape("paddle")
+
+L.setx(play_left + 10)
+R.setx(play_right - 10)
+
+# Score
+score = Turtle()
+score.hideturtle()
+score.penup()
+
+score_L = 0
+score_R = 0
+
+def write_score():
+    score.clear()
+    score.goto(-screen.window_width() / 4, screen.window_height() / 2 - 80)
+    score.write(score_L, align="center", font=("Arial", 32, "bold"))
+    score.goto(screen.window_width() / 4, screen.window_height() / 2 - 80)
+    score.write(score_R, align="center", font=("Arial", 32, "bold"))
+
+write_score()
